bc6f206ecdbc2745f07f33935d870e8e1849f759
Janodi Jayathunga <79194620+20061205@users.noreply.github.com>
2024-10-01 09:36:43 +0530

    Added app folder and its contents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './cards.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CheckoutPage from './checkout.jsx';
+import HomePage from './home.jsx';
+import Guest from './homeComponent/loginpage/guest.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="api/cart" element={<Card />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/guest" element={<Guest/>} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
