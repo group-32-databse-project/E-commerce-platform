0cb9e971b343615f3fe1f1634429b5e2bcba6aa9
Parawork <154836766+Parawork@users.noreply.github.com>
2024-10-06 03:52:57 +0530

    Revert "Dev"

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,13 +2,9 @@ import "./assets/styles/App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Card from "./pages/cards.jsx";
-import CheckoutPage from "./pages/checkout/Checkout.js";
+import CheckoutPage from "./pages/checkout.jsx";
 import HomePage from "./pages/Dashboard/home.jsx";
-import SignIn from "./pages/sign/sign-in/SignIn.jsx";
-import SignUp from "./pages/sign/sign-up/signup.jsx";
-import Cart from "./pages/cart/cart.jsx";
-import About from "./pages/about/about.jsx"
-import Contact from "./pages/contact/contact.jsx";
+import Guest from "./pages/loginpage/guest.jsx";
 
 function App() {
   return (
@@ -17,12 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="api/cart" element={<Card />} />
         <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/carts" element={<Cart />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        
+        <Route path="/guest" element={<Guest />} />
       </Routes>
     </Router>
   );
